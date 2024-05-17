1035cb677fd68b19a976181016a4abaecfbc9f29
Mpumzi Mbula <127626985+20Mphumzi16@users.noreply.github.com>
2024-05-17 23:57:51 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/factory/CartFactory.java
@@ -0,0 +1,22 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Cart;
+import za.ac.cput.domain.ComicBook;
+import za.ac.cput.domain.Wishlist;
+import za.ac.cput.util.Helper;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class CartFactory {
+    public static Cart buildCart(double totalPrice, List<ComicBook> comicBooks, LocalDate createdDate, LocalDate updatedDate) {
+        if (Helper.isValidPrice(totalPrice) || Helper.isComicBooksListNullOrEmpty(comicBooks) || Helper.isNotAfter(createdDate, LocalDate.now()) || Helper.isNotInPast(updatedDate)) {
+            return null;
+        }
+        return new Cart.Builder().setTotalPrice(totalPrice)
+                .setComicBookList(comicBooks)
+                .setCreatedDate(createdDate)
+                .setUpdatedDate(updatedDate)
+                .build();
+    }
+}
--- src/main/java/za/ac/cput/factory/WishlistFactory.java
+++ src/main/java/za/ac/cput/factory/WishlistFactory.java
@@ -1,4 +1,24 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.ComicBook;
+import za.ac.cput.domain.Wishlist;
+import za.ac.cput.util.Helper;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class WishlistFactory {
+    public static Wishlist buildWishlist(String wishListName, List<ComicBook> comicBooks, LocalDate createdDate, LocalDate updatedDate) {
+        if (Helper.isStringNullorEmpty(wishListName) || Helper.isComicBooksListNullOrEmpty(comicBooks) || Helper.isNotAfter(createdDate, LocalDate.now()) || Helper.isNotInPast(updatedDate)) {
+            return null;
+        }
+
+        return new Wishlist.Builder()
+                .setWishlistName(wishListName)
+                .setComicBooks(comicBooks)
+                .setCreatedDate(createdDate)
+                .setUpdatedDate(updatedDate)
+                .build();
+    }
+
 }
